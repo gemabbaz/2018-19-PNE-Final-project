b3c61b20714213a7661eac66c1212b5bba93c1b3
gemabbaz <gemabb00@gmail.com>
2019-04-03 17:42:12 +0200

    Starting the final project :)

--- prueba (NO VALIDO).py
+++ prueba (NO VALIDO).py
@@ -1,9 +1,26 @@
-import http.client
-import json
-import socketserver
+def karyotype_function(ENDPOINT2):
+    conn = http.client.HTTPSConnection(HOSTNAME)
+    type = input("Introduce the specie you want to know the karyotype of: ")
+    conn.request(METHOD, ENDPOINT2+type+"?content-type=application/json", None, headers)
+    # -- Print the status
+    r1 = conn.getresponse()
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
+    text_json = r1.read().decode("utf-8")
+    karyotype = json.loads(text_json)
+    list2 = karyotype["top_level_region"]
+    list2_names = []
+    for i in range(len(list2)):
+        list2_names.append(list2[i]["name"])
 
-# Define the Server's port
-PORT = 8002
+    return list2_names
+
+info2 = karyotype_function(ENDPOINT2)
+for i in info2:
+    print("\t", i, end = "\n")
+
+
 
 # -- API information
 HOSTNAME = "rest.ensembl.org"
@@ -12,17 +29,6 @@ ENDPOINT2 = "/info/assembly/"
 ENDPOINT3 = "/info/assembly/"
 
 METHOD = "GET"
-
-class TestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("GET received")
-        print("Request line" + self.requestline)
-        print("       Cmd:  " + self.command)
-        print("Path:  " + self.path)
-        if self.path == "/":
-            file = open("main.html")
-            content = file.read()
-            file.close()
 
 # -- Here we can define special headers if needed
 headers = {'User-Agent': 'http-client'}
